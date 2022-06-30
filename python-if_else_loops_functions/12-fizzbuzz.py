6d0bba20d0fb5d92c2e30848e18324f388f5544b
root <o.olurinola@alustudent.com>
2022-06-30 05:05:09 +0200

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for 1 in range(1, 101):
-        if 1 % 3 == 0 and i % 5 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
