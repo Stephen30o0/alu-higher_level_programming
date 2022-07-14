939059662793c6624956dc5d7c550884875c25dd
root <o.olurinola@alustudent.com>
2022-07-14 05:05:38 +0200

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    00_list = my_list[:]
+    new_list = my_list[:]
     for i in range(len(my_list)):
         if my_list[i] == search:
-            00_list[i] = replace
-    return 00_list
+            new_list[i] = replace
+    return new_list
