00a60460f0ab4446ec448543a69c0b51a3a8fe94
root <o.olurinola@alustudent.com>
2022-07-16 01:01:31 +0200

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    return max(a_dictionary)
+    flag = 1
+    for key in a_dictionary:
+        if flag == 1:
+            largestVal = a_dictionary[key]
+            largestKey = key
+            flag = 0
+        if a_dictionary[key] > largestVal:
+            largestVal = a_dictionary[key]
+            largestKey = key
+    return largestKey
