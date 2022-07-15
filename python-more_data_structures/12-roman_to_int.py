6ecd54c01a744abd7d33694bd23cf076f507544e
root <o.olurinola@alustudent.com>
2022-07-15 14:29:08 +0200

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,10 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    romandict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000}
+    romandict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
     n = 0
     for j in range(len(roman_string)):
-        if j > 0 and romandict[roman_string[j]] >
-        romandict[roman_string[j - 1]]:
+        if j > 0 and romandict[roman_string[j]] > romandict[roman_string[j - 1]]:
             n += romandict[roman_string[j]] - 2 * \
                         romandict[roman_string[j - 1]]
         else:
