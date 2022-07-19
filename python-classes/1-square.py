487d76bdbc4e8c38f153771b8de9f3a91d9244e4
root <o.olurinola@alustudent.com>
2022-07-19 22:53:43 +0200

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""NO MODULES USED"""
+
+
+class Square:
+    def __init__(self,size):
+        """intializes the attributes of Square"""
+        """Making "size" a private instance attribute"""
+        self.size = size
+
