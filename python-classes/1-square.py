9f9f3e096eaf7c8417a34690ab4d3168c6871a9b
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2022-07-20 14:17:06 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""NO MODULES USED IN THIS CODE"""
+"""
+NO MODULES USED IN THIS CODE
+"""
 
 
 class Square:
-    """intializes the attributes of Square
-    Making size a private instance attribute"""
+    """
+    intializes the attributes of Square
+    Making size a private instance attribute
+    """
      def __init__(self, size):
         self.square_size = size
