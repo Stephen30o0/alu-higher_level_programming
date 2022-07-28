d0e8a663476a32e09589f7fd256987d453212714
root <o.olurinola@alustudent.com>
2022-07-28 20:07:11 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 6 with a class
+"""
+
+
+class BaseGeometry:
+    """Class with public instance method."""
+
+    def area(self):
+        raise Exception('area() is not implemented')
