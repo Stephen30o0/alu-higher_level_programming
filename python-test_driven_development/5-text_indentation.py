2b67499d03ccb9897cc8271a134ee89e95a0bcd9
Stephen30o0 <o.olurinola@alustudent.com>
2022-08-15 17:21:31 +0200

    Task 6

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,13 @@
 def text_indentation(text):
+    """Prints text with added 2 newlines
+    after each of these characters ('.', '?', ':').
+    """
+
     if type(text) is not str:
         raise TypeError("text must be a string")
-    for i in range(len(text)):
-        if text[i] == '.' or text[i] == '?' \
-                or text[i] == ':':
-            print("{}\n".format(text))
 
+    for i in ".:?":
+        text = (i + "\n\n").join(
+            [line.strip(" ") for line in text.split(i)])
 
-
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+    print("{}".format(text), end="")
