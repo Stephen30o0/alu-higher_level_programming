5dac51eb77515317b7fb04cf54b72383b6d47108
Stephen30o0 <o.olurinola@alustudent.com>
2022-08-15 17:23:57 +0200

    Task 6

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+Module text_indentation
+Adds 2 new lines after a certain set characters.
+"""
+
+
 def text_indentation(text):
     """Prints text with added 2 newlines
     after each of these characters ('.', '?', ':').
