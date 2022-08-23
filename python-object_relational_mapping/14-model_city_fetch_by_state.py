f902350a56350d03deaaa4bb58b2d25d29f4e739
Stephen30o0 <o.olurinola@alustudent.com>
2022-08-23 13:04:03 +0200

    fix//'';

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,22 +3,19 @@
 from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
-
-    import sys
+    from sys import argv
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+    from sqlalchemy.orm import aliased, sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker()
+    session = Session(bind=engine)
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(City.state_id == State.id)\
-                              .order_by(City.id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    res = (session.query(State, City).filter(
+        State.id == City.state_id).order_by(City.id).all())
+    for st, cy in res:
+        print("{}: ({:d}) {}".format(st.name, cy.id, cy.name))
     session.close()
