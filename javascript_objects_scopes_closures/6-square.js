6a427c1a8025d67456e1c1386a3486e71d254d28
Stephen30o0 <o.olurinola@alustudent.com>
2022-10-06 17:42:54 +0200

    fixup

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-const square = require('./5-square');
-
-class Square extends square {
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-      console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
-    } else {
-      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
-}
-module.exports = Square;
+};
